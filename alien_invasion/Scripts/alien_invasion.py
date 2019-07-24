f10502bed9f8ddbfa851481efbd1574ea8d76e73
hello-word <y793796203@gmail.com>
2019-07-24 13:44:35 +0800

    second commit

--- alien_invasion/Scripts/alien_invasion.py
+++ alien_invasion/Scripts/alien_invasion.py
@@ -7,6 +7,7 @@ from pygame.sprite import Group
 from alien import Alien
 from game_states import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 def run_game():
 
@@ -20,6 +21,7 @@ def run_game():
     aliens = Group()
     gf.create_fleet(ai_settings,screen,ship,aliens)
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings,screen,stats)
     while True:
         gf.check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets)
         #gf.check_events()
@@ -28,8 +30,16 @@ def run_game():
             gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
             gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
 
-        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
+        gf.update_screen(ai_settings,screen,stats,sb,ship,aliens,bullets,play_button)
 run_game()
+
+
+
+
+
+
+
+
 
 
 
